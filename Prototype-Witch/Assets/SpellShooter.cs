7924c0971516f99b5e41e17ff90a356708912229
Jonathan <jote962010@hotmail.com>
2020-08-20 16:49:54 +0200

    Added the start of a spell shooter

--- Prototype-Witch/Assets/SpellShooter.cs
+++ Prototype-Witch/Assets/SpellShooter.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SpellShooter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject spellObject = null;
+    
+    private Vector3 shootDirection = Vector3.forward;
+    private int numberOfSpellsFired;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            numberOfSpellsFired++;
+            if (numberOfSpellsFired < 2)
+            {
+                var spell = Instantiate(spellObject, transform.position + transform.rotation * new Vector3(0, 0, 4), Quaternion.identity);
+                spell.GetComponent<Rigidbody>().velocity = Vector3.forward * 20;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            numberOfSpellsFired = 0;
+        }
+
     }
 }
